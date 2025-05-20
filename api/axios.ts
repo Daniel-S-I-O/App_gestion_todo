c29fbf6938ba085992ac21aaa57fae145e117409
DANIEL <est.dsinestrozao0201@uml.edu.ni>
2025-05-20 08:22:02 -0600

    axios

--- api/axios.ts
+++ api/axios.ts
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://apiexamenfinal-production.up.railway.app/api/', // Cambia por tu baseURL
+  baseURL: 'https://apiexamenfinal-production.up.railway.app/api/',
 });
 
 api.interceptors.request.use(async (config) => {
